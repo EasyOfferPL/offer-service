9d0f0098ccd40530b0ec1c59033bb24586d00602
BohdanMoldavets <143205638+BohdanMoldavets@users.noreply.github.com>
2025-12-12 10:12:47 +0100

    fix: sonar issues

--- src/main/java/pl/easyoffer/offer_service/model/OfferContext.java
+++ src/main/java/pl/easyoffer/offer_service/model/OfferContext.java
@@ -6,6 +6,7 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.FieldNameConstants;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.Set;
 
@@ -14,7 +15,7 @@ import java.util.Set;
 @AllArgsConstructor
 @EqualsAndHashCode
 @FieldNameConstants
-public class OfferContext {
+public class OfferContext implements Serializable {
 
     private LocalDate publishDate;
     private String title;
